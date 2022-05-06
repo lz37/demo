83614d2b6fce726e801dfa5f4b7162b97eeee6c4
zerozawa <zekaixie37@gmail.com>
2022-05-06 11:12:14 +0800

    响应式布局项目再次加了一点点细节

--- 响应式布局/style.js
+++ 响应式布局/style.js
@@ -1,15 +1,12 @@
 window.onload = function () {
   var imgs = document.getElementsByTagName("img");
-  var width = imgs[1].style.width;
-  var marginLeft = imgs[1].style.marginLeft;
+  var body = document.getElementsByTagName("body")[0];
   for (let index = 1; index < imgs.length; index++) {
     imgs[index].onmouseover = function () {
-      this.style.width = "100%";
-      this.style.marginLeft = "0%";
+      body.style.backgroundImage = "url(" + imgs[index].src + ")";
     };
     imgs[index].onmouseleave = function () {
-      this.style.width = width;
-      this.style.marginLeft = marginLeft;
+      body.style.background = "";
     };
   }
 };
